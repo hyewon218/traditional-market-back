bd6acfe4eedf8b64b8bc4ebb730844a54c6dd33c
hyewon218 <won_11111@naver.com>
2024-05-15 22:20:18 +0900

    Initial commit

---
+++ src/main/java/com/market/traditionalmarketback/TraditionalMarketBackApplication.java
@@ -0,0 +1,13 @@
+package com.market.traditionalmarketback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraditionalMarketBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraditionalMarketBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/market/traditionalmarketback/TraditionalMarketBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.traditionalmarketback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraditionalMarketBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
