18f7631b7efa29ae7a9124bf718d22ab63090710
hyewon218 <won_11111@naver.com>
2024-05-16 17:03:47 +0900

    Update README.md

---
+++ src/main/java/com/market/TraditionalMarketBackApplication.java
@@ -0,0 +1,12 @@
+package com.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraditionalMarketBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraditionalMarketBackApplication.class, args);
+    }
+}
---
+++ src/main/java/com/market/domain/item/constant/ItemSellStatus.java
@@ -0,0 +1,5 @@
+package com.market.domain.item.constant;
+
+public enum ItemSellStatus {
+    SELL, SOLD_OUT
+}
