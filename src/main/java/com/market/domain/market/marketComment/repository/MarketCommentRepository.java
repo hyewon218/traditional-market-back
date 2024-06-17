7d4979168ff6282ee0cc78c528e1925da3d45f27
hyewon218 <won_11111@naver.com>
2024-06-17 23:37:32 +0900

    [Refactor] 시장 댓글 조회 페이징 처리

--- src/main/java/com/market/domain/market/marketComment/repository/MarketCommentRepository.java
+++ src/main/java/com/market/domain/market/marketComment/repository/MarketCommentRepository.java
@@ -1,8 +1,10 @@
 package com.market.domain.market.marketComment.repository;
 
 import com.market.domain.market.marketComment.entity.MarketComment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MarketCommentRepository extends JpaRepository<MarketComment, Long> {
-
+    Page<MarketComment> findAllByMarket_No(Long no, Pageable pageable);
 }
--- src/main/java/com/market/domain/market/marketComment/service/MarketCommentService.java
+++ src/main/java/com/market/domain/market/marketComment/service/MarketCommentService.java
@@ -1,12 +1,17 @@
 package com.market.domain.market.marketComment.service;
 
 import com.market.domain.market.marketComment.dto.MarketCommentRequestDto;
+import com.market.domain.market.marketComment.dto.MarketCommentResponseDto;
 import com.market.domain.market.marketComment.entity.MarketComment;
 import com.market.domain.member.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface MarketCommentService {
 
     void createMarketComment(MarketCommentRequestDto itemCommentRequestDto, Member member);
+
+    Page<MarketCommentResponseDto> getMarketComments(Long marketNo, Pageable pageable);
 
     void updateMarketComment(Long commentNo, MarketCommentRequestDto marketCommentRequestDto,
         Member member);
