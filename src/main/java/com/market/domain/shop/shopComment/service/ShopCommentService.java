c3d469ec36c854bf37412ebfe55d6710a3be5fba
hyewon218 <won_11111@naver.com>
2024-06-18 19:18:03 +0900

    [Refactor] 상점 댓글 조회 페이징 처리

--- src/main/java/com/market/domain/shop/shopComment/service/ShopCommentService.java
+++ src/main/java/com/market/domain/shop/shopComment/service/ShopCommentService.java
@@ -2,11 +2,16 @@ package com.market.domain.shop.shopComment.service;
 
 import com.market.domain.member.entity.Member;
 import com.market.domain.shop.shopComment.dto.ShopCommentRequestDto;
+import com.market.domain.shop.shopComment.dto.ShopCommentResponseDto;
 import com.market.domain.shop.shopComment.entity.ShopComment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ShopCommentService {
 
     void createShopComment(ShopCommentRequestDto shopCommentRequestsDto, Member member);
+
+    Page<ShopCommentResponseDto> getShopComments(Long marketNo, Pageable pageable);
 
     void updateShopComment(Long commentNo, ShopCommentRequestDto shopCommentRequestsDto,
         Member member);
