ced6adbdd6e05c54f2a35020fb458cb25e336770
hyewon218 <won_11111@naver.com>
2024-06-18 19:30:09 +0900

    [Refactor] 상품 댓글 조회 페이징 처리

--- src/main/java/com/market/domain/item/itemComment/service/ItemCommentService.java
+++ src/main/java/com/market/domain/item/itemComment/service/ItemCommentService.java
@@ -1,12 +1,17 @@
 package com.market.domain.item.itemComment.service;
 
 import com.market.domain.item.itemComment.dto.ItemCommentRequestDto;
+import com.market.domain.item.itemComment.dto.ItemCommentResponseDto;
 import com.market.domain.item.itemComment.entity.ItemComment;
 import com.market.domain.member.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ItemCommentService {
 
     void createItemComment(ItemCommentRequestDto itemCommentRequestDto, Member member);
+
+    Page<ItemCommentResponseDto> getItemComments(Long itemNo, Pageable pageable);
 
     void updateItemComment(Long commentNo, ItemCommentRequestDto itemCommentRequestDto,
         Member member);
