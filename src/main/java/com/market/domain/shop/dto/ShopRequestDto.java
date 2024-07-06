c5729125f39eebc1126551caf7da1ad821d93b40
hyewon218 <won_11111@naver.com>
2024-07-06 14:31:13 +0900

    [Fix typo] 오타 수정

--- src/main/java/com/market/domain/shop/dto/ShopRequestDto.java
+++ src/main/java/com/market/domain/shop/dto/ShopRequestDto.java
@@ -28,7 +28,7 @@ public class ShopRequestDto {
     @NotBlank(message = "상점명은 필수 입력 값입니다.")
     private String shopName;
 
-    @NotBlank(message = "상점 번호는 필수 입력 값입니다.")
+    @NotBlank(message = "상점 전화번호는 필수 입력 값입니다.")
     @Pattern(regexp = "^01(?:0|1|[6-9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$", message = "10 ~ 11 자리의 숫자만 입력 가능합니다.")
     private String tel;
 
