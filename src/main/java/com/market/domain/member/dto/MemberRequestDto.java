efd5c0557b14bc44d7811fa61edfe6248ddb4304
Woncheol SONG <133622405+songwc3@users.noreply.github.com>
2024-07-11 22:41:10 +0900

    [Refactor] 기존 memberNickname에서 randomTag를 조합하여 닉네임 중복 문제 해결 (#85)

--- src/main/java/com/market/domain/member/dto/MemberRequestDto.java
+++ src/main/java/com/market/domain/member/dto/MemberRequestDto.java
@@ -18,18 +18,28 @@ public class MemberRequestDto {
     private String memberId;
 
     @NotBlank(message = "이메일은 반드시 입력해주세요")
-    private String memberEmail;
+    private String memberEmail; // 아래 emailId와 domainCustom 합쳐서 저장, 프론트에서 처리
+
+    private String emailId;
+    private String domainCustom;
 
     private String memberNickname;
+    private String randomTag; // # + 0~9까지 랜덤으로 생성되는 숫자 7자리
+    private String nicknameWithRandomTag; // 닉네임 + 랜덤태그, ex) 닉네임#1234567
 
     @NotBlank(message = "비밀번호는 반드시 입력해주세요")
     private String memberPw;
 
+    @NotBlank(message = "비밀번호 확인을 반드시 입력해주세요")
+    private String confirmPw;
+
+    // 테스트에서만 사용함
     public MemberRequestDto(String modifiedMemberNickname, String modifiedMemberPw) {
         this.memberNickname = modifiedMemberNickname;
         this.memberPw = modifiedMemberPw;
     }
 
+    // 테스트에서만 사용함
     public MemberRequestDto(String memberId, String memberEmail, String memberPw) {
         this.memberId = memberId;
         this.memberEmail = memberEmail;
@@ -37,13 +47,30 @@ public class MemberRequestDto {
     }
 
     public Member toEntity(PasswordEncoder passwordEncoder){
+        String randomTag = generateRandomTag();
+
         return Member.builder()
                 .memberId(this.memberId)
                 .memberEmail(this.memberEmail)
                 .memberNickname(this.memberNickname)
+                .randomTag(randomTag)
+                .nicknameWithRandomTag(this.memberNickname + randomTag)
                 .memberPw(passwordEncoder.encode(this.memberPw))
                 .role(Role.MEMBER)
                 .providerType(ProviderType.LOCAL)
                 .build();
+    }
+
+    // 닉네임 뒤 #으로 시작하는 랜덤태그 생성
+    private String generateRandomTag() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("#"); // "#"으로 시작하도록 추가
+
+        // 나머지 자리에는 0부터 9까지의 숫자를 랜덤하게 생성
+        for (int i = 0; i < 7; i++) {
+            int digit = (int) (Math.random() * 10);
+            sb.append(digit);
+        }
+        return sb.toString();
     }
 }
--- src/main/java/com/market/domain/member/entity/Member.java
+++ src/main/java/com/market/domain/member/entity/Member.java
@@ -2,7 +2,6 @@ package com.market.domain.member.entity;
 
 import com.market.domain.base.BaseEntity;
 import com.market.domain.cart.entity.Cart;
-import com.market.domain.inquiry.entity.Inquiry;
 import com.market.domain.member.constant.Role;
 import com.market.domain.order.entity.Order;
 import com.market.global.security.oauth2.ProviderType;
@@ -29,10 +28,15 @@ public class Member extends BaseEntity {
     @Column(nullable = false, unique = true)
     private String memberId;
 
-    private String memberNickname;
-
     @Column(nullable = false, unique = true)
     private String memberEmail;
+
+    private String memberNickname;
+
+    private String randomTag;
+
+    @Column(nullable = false)
+    private String nicknameWithRandomTag;
 
     @Column(nullable = false)
     private String memberPw;
@@ -49,9 +53,6 @@ public class Member extends BaseEntity {
 
     @Builder.Default
     @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
-    private List<Inquiry> inquiries = new ArrayList<>();
-
-    @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
     private List<Order> orders = new ArrayList<>();
 
     public Member(String memberId, String memberEmail, String memberNickname, String memberPw, ProviderType providerType, Role role) {
@@ -63,14 +64,16 @@ public class Member extends BaseEntity {
         this.role = role;
     }
 
-    // 회원 정보 수정 메서드
+    // 회원 정보 수정 메서드(닉네임, 비밀번호 수정)
     public void update(String memberNickname, String memberPw) {
         this.memberNickname = memberNickname;
+        this.nicknameWithRandomTag = memberNickname + this.randomTag;
         this.memberPw = memberPw;
     }
 
     public void updateNickname(String memberNickname) {
         this.memberNickname = memberNickname;
+        this.nicknameWithRandomTag = memberNickname + this.randomTag;
     }
 
     // OAuth2 로그인 시 해당 사이트의 email과 다르다면 해당 사이트 이메일로 업데이트
