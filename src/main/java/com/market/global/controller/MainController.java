93a55ab9670c24d7e42717f5d68533584f0c1984
songwc3 <songwc3@gmail.com>
2024-07-13 11:32:49 +0900

    [Front] html, css 추가

--- src/main/java/com/market/global/controller/MainController.java
+++ src/main/java/com/market/global/controller/MainController.java
@@ -1,16 +1,26 @@
 package com.market.global.controller;
 
+import com.market.domain.inquiry.entity.Inquiry;
+import com.market.domain.inquiry.service.InquiryServiceImpl;
+import com.market.domain.member.entity.Member;
 import com.market.domain.member.service.MemberServiceImpl;
+import com.market.global.security.UserDetailsImpl;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class MainController {
 
     private final MemberServiceImpl memberService;
+    private final InquiryServiceImpl inquiryService;
 
     @GetMapping("/")
     public String mainPage() {
@@ -67,10 +77,65 @@ public class MainController {
         return "cart/main";
     }
 
-    // 내정보 페이지
+    // 내정보 페이지 홈 // 인증 필요, 테스트 끝나면 security에서 바꾸기
     @GetMapping("/myinfo")
     public String myinfoMain() {
         return "members/myinfo";
+    }
+
+    // 내정보 // 인증 필요, 테스트 끝나면 security에서 바꾸기
+    @GetMapping("/myinfo/myinfo")
+    public String getMyInfo(@AuthenticationPrincipal UserDetailsImpl userDetails, HttpServletRequest request) {
+        String randomTag = userDetails.getMember().getRandomTag();
+        if (memberService.isPasswordVerified(request, randomTag)) {
+            return "members/myinfo/myinfo";
+        } else {
+            return "members/myinfo/checkpassword";
+        }
+    }
+
+    // 구매목록 // 인증 필요, 테스트 끝나면 security에서 바꾸기
+    @GetMapping("/myinfo/list")
+    public String getMypurchaseList() {
+        return "members/myinfo/list";
+    }
+
+    // 배송지 관리 // 인증 필요, 테스트 끝나면 security에서 바꾸기
+    @GetMapping("/myinfo/delivery")
+    public String getMyDeliveryList() {
+        return "members/myinfo/mydelivery";
+    }
+
+    // 내 문의사항 목록 보기 // 인증 필요, 테스트 끝나면 security에서 바꾸기
+    @GetMapping("/myinfo/inquiries")
+    public String getMyInquiryList() {
+        return "members/myinfo/inquiries";
+    }
+
+    // 내 문의사항 개별 보기 // 인증 필요, 테스트 끝나면 security에서 바꾸기
+    @GetMapping("/myinfo/inquiry/{inquiryNo}")
+    public String getMyInquiry(@PathVariable Long inquiryNo,
+                               @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Member member = userDetails.getMember();
+        Inquiry inquiry = inquiryService.findById(inquiryNo);
+
+        if (member.getMemberNo() == inquiry.getMemberNo()) {
+            return "inquiry/inquiryinfo";
+        } else {
+            return "unauthorized";
+        }
+    }
+
+    // 배송지 관리
+    @GetMapping("/myinfo/deliveries")
+    public String getDeliveries() {
+        return "members/myinfo/mydelivery";
+    }
+
+    // 회원 탈퇴
+    @GetMapping("/myinfo/v")
+    public String getVerification() {
+        return "members/myinfo/delete";
     }
 
     // 시장 찾기 메뉴
