bb738a9868153372c3f7cbc6ae414441d9d9d4a6
hyewon218 <won_11111@naver.com>
2024-07-29 23:48:10 +0900

    [Refactor] 오타수정 및 코드 리팩토링

--- src/main/java/com/market/domain/market/controller/MarketController.java
+++ src/main/java/com/market/domain/market/controller/MarketController.java
@@ -7,17 +7,15 @@ import com.market.domain.market.repository.MarketSearchCond;
 import com.market.domain.market.service.MarketService;
 import com.market.global.response.ApiResponse;
 import com.market.global.security.UserDetailsImpl;
+import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.List;
-
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -52,9 +50,9 @@ public class MarketController {
         return ResponseEntity.ok().body(result);
     }
 
-    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/markets/search") // 키워드 검색 시장 목록 조회
-    public ResponseEntity<Page<MarketResponseDto>> searchMarkets(MarketSearchCond cond, Pageable pageable){
+    public ResponseEntity<Page<MarketResponseDto>> searchMarkets(MarketSearchCond cond,
+        Pageable pageable) {
         Page<MarketResponseDto> result = marketService.searchMarkets(cond, pageable);
         return ResponseEntity.ok().body(result);
     }
@@ -69,7 +67,7 @@ public class MarketController {
 
     @GetMapping("/markets/{marketNo}") // 시장 단건 조회
     public ResponseEntity<MarketResponseDto> getMarket(
-            @PathVariable("marketNo") Long marketNo, HttpServletRequest request) {
+        @PathVariable("marketNo") Long marketNo, HttpServletRequest request) {
         return ResponseEntity.ok(marketService.getMarket(marketNo, request));
     }
 
