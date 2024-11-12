aee531d4ca6f2b0499e94ea20f0eb4833c31004b
Hyewon Choi <won_11111@naver.com>
2024-11-13 01:51:02 +0900

    [Refactor] Kafka Consumer 설정 및 리밸런싱 리스너 구현 (#212)

--- src/main/java/com/market/domain/kafka/config/KafkaConsumerConfig.java
+++ src/main/java/com/market/domain/kafka/config/KafkaConsumerConfig.java
@@ -1,9 +1,13 @@
 package com.market.domain.kafka.config;
 
 import com.market.domain.notification.entity.NotificationEvent;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -11,18 +15,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConsumerAwareRebalanceListener;
 import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.util.backoff.FixedBackOff;
 
 //@EnableKafka
 @Configuration
+@Slf4j
 public class KafkaConsumerConfig {
+
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
     @Value("${spring.kafka.consumer.auto-offset-reset}")
     private String autoOffsetResetConfig;
     @Value("${spring.kafka.consumer.group-id}")
     private String groupId;
+
     @Bean
     public ConsumerFactory<Long, NotificationEvent> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
@@ -32,7 +42,8 @@ public class KafkaConsumerConfig {
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetResetConfig);
 
-        JsonDeserializer<NotificationEvent> jsonDeserializer = new JsonDeserializer<>(NotificationEvent.class);
+        JsonDeserializer<NotificationEvent> jsonDeserializer = new JsonDeserializer<>(
+            NotificationEvent.class);
         jsonDeserializer.addTrustedPackages("*");
         return new DefaultKafkaConsumerFactory<>(props, new LongDeserializer(), jsonDeserializer);
     }
@@ -41,8 +52,32 @@ public class KafkaConsumerConfig {
     public ConcurrentKafkaListenerContainerFactory<Long, NotificationEvent> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<Long, NotificationEvent> factory =
             new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConcurrency(3); // 컨슈머 스레드 수 설정(파티션 수와 동일하게 설정)
         factory.setConsumerFactory(consumerFactory());
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+
+        // 에러 핸들러 추가
+        factory.setCommonErrorHandler(new DefaultErrorHandler(
+            new FixedBackOff(1000L, 2) // 1초 간격으로 최대 2회 재시도
+        ));
+
+        // 리스너 컨테이너 이벤트 리스너
+        factory.getContainerProperties()
+            .setConsumerRebalanceListener(new ConsumerAwareRebalanceListener() {
+                @Override
+                public void onPartitionsRevokedBeforeCommit(Consumer<?, ?> consumer,
+                    Collection<TopicPartition> partitions) {
+                    // 파티션 해제 전 오프셋 커밋
+                    consumer.commitSync(); // 비동기식(commitAsync) 사용 시 데이터 유실 가능
+                }
+
+                @Override
+                public void onPartitionsAssigned(Consumer<?, ?> consumer,
+                    Collection<TopicPartition> partitions) {
+                    // 파티션 할당 후 처리
+                    log.info("Partitions assigned: {}", partitions);
+                }
+            });
 
         return factory;
     }
